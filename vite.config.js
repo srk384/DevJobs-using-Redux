d36a9cb272bb4a5cfdc409b17f4feb28c59e150f
srk384 <khanshahrukh384@gmail.com>
2025-06-17 16:08:51 +0530

    chore: Update Vite configuration for consistent formatting and add Vercel rewrites configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/',
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
   plugins: [react(), tailwindcss()],
   server: {
-    host: '0.0.0.0',  // Allows access from other devices
-    port: 5173,       // Use your preferred port
+    host: "0.0.0.0", // Allows access from other devices
+    port: 5173, // Use your preferred port
   },
-})
+});
